6132cb549011f4aad5a2851a70d9646119874258
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-06-14 12:24:45 +0530

    Finished Backend.

--- blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/config/SwaggerConfig.java
+++ blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/config/SwaggerConfig.java
@@ -11,10 +11,6 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 @Configuration
 public class SwaggerConfig {
 
