9f5e4d62d79b32b5dc573025e90b1aa1a97021fc
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-06-17 16:26:35 +0530

    make some changes to handle errors in frontend.

--- blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/payloads/UserDto.java
+++ blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/payloads/UserDto.java
@@ -17,18 +17,19 @@ import java.util.Set;
 public class UserDto {
     private int id;
 
-    @NotEmpty
+//    @NotEmpty(message = "Username is Required !!")
     @Size(min = 4, message = "Username must be min of 4 characters")
     private String name;
 
     @Email(message = "Email address is not valid !!")
+    @NotEmpty(message = "Email is required !!")
     private String email;
 
     @NotEmpty
     @Size(min = 3,max = 10, message = "Password must be min of 3 chars and max of 10 chars !!")
     private String password;
 
-    @NotEmpty
+    @NotEmpty(message = "About is Required !!")
     private String about;
 
     private Set<RoleDto> roles = new HashSet<>();
