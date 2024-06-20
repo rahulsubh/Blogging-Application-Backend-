55957feab0a157473d75ec6627e9491e7256d14b
rahulsubh <102258593+rahulsubh@users.noreply.github.com>
2024-06-20 16:39:50 +0530

    some url mapping changes done according to frontend.

--- blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/config/SecurityConfig.java
+++ blog-app-apis/blog-app-apis/src/main/java/com/rahul/blog/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -60,6 +61,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth ->
                                 auth.requestMatchers(PUBLIC_URLS)
+                                        .permitAll()
+                                        .requestMatchers(HttpMethod.GET)
                                         .permitAll()
                                         .anyRequest()
                                         .authenticated()
